89d1cc23f2f78b26eabb2590bd7dfa6c3a9ee24c
nazar.lynovetsky@skynix.co <nazar.lynovetsky@skynix.co>
2019-12-16 18:20:42 +0200

    Added google maps.

--- plugins/globalComponents/general.js
+++ plugins/globalComponents/general.js
@@ -1,19 +1,28 @@
 import Vue from "vue"
 
+import Container from "~/components/Layout/Container"
+import List from "~/components/Layout/List"
+
 import Section from "~/components/Section/Section"
 import SectionHeader from "~/components/Section/SectionHeader"
 import SectionFooter from "~/components/Section/SectionFooter"
-import Container from "~/components/Layout/Container"
-import List from "~/components/Layout/List"
+
+import Breadcrumbs from "~/components/Page/Breadcrumbs"
+
 import Button from "~/components/UI/Button"
 
 
 // Layout
+Vue.component('Container', Container)
+Vue.component('List', List)
+
+// Section
 Vue.component('Section', Section)
 Vue.component('SectionHeader', SectionHeader)
 Vue.component('SectionFooter', SectionFooter)
-Vue.component('Container', Container)
-Vue.component('List', List)
+
+// Page
+Vue.component('Breadcrumbs', Breadcrumbs)
 
 // UI
 Vue.component('Button', Button)
