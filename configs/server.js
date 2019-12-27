38ebbcedf1a802369cecbaca68a5b5ce98a86c8c
nazar.lynovetsky@skynix.co <nazar.lynovetsky@skynix.co>
2019-12-27 15:19:33 +0200

    Added configs files.

---
+++ configs/server.js
@@ -0,0 +1,10 @@
+/*
+port: 3000,
+host: 'localhost'
+*/
+
+
+export default {
+    port: 3000,
+    host: 'localhost'
+  }
--- plugins/modules/axios.js
+++ plugins/modules/axios.js
@@ -3,7 +3,7 @@ import axios from 'axios'
 const OPTIONS = {
     baseURL: process.env.BASE_API_URL,
     headers: {
-        'Content-Type': 'multipart/form-data'
+        'Content-Type': 'application/json'
     }
 }
 
