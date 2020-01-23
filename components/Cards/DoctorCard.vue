a6ee36fa636e286589d051a655840b2683a34ef4
andrei.volosovich@skynix.co <andrei.volosovich@skynix.co>
2020-01-23 10:37:05 +0200

    OH-73-Create-Home-Page-Layout (init)

--- components/Cards/DoctorCard.vue
+++ components/Cards/DoctorCard.vue
@@ -2,11 +2,13 @@
     <NuxtLink :to="'/'" class="link link--card">
         <div class="doctor-card">
             <header class="doctor-card__header">
-                <img src="" alt="">
+                <img src="https://placeimg.com/352/480/people?t=1579768204582" alt="doctor image">
             </header>
             <div class="doctor-card__main">
+                
                 <h3 class="doctor-card__title">Doctor Name</h3>
-                <p class="doctor-card__text">Doctor description abot self.</p>
+                
+                <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Iusto ullam labore dolor, laboriosam autem officiis ipsam incidunt voluptas, quidem reprehenderit vero consequatur, sint deleniti corporis sapiente deserunt provident adipisci reiciendis.</p>
             </div>
         </div>
     </NuxtLink>
@@ -26,5 +28,12 @@ export default {
 <style lang="scss">
 .doctor-card {
     font-family: 'Sans';
+
+    &__header{
+        img{
+            max-width: 352px;
+        }
+    }
+
 }
 </style>
