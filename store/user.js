715580c69feb62791fdeaf9356d052092966256a
Evgeniy_Galyaev <gep.for.work@gmail.com>
2020-02-24 17:00:31 +0200

    OH-69: add base logic and layout for connect stripe account to user(doctor) profile

--- store/user.js
+++ store/user.js
@@ -1,113 +1,119 @@
-// services
-import UserApi from '../services/api/User'
+import UserApi from "../services/api/User";
 
 // options (init values)
-const DEFAULT_USER = null
-
+const DEFAULT_USER = null;
 
 export const state = () => ({
-    user: DEFAULT_USER,
-})
+    user : DEFAULT_USER,
+    userPaymentData: null
+});
 
 export const mutations = {
-    SET_USER (state, user) {
-        state.user = user
+    SET_USER(state, user){
+        state.user = user;
     },
-}
-
+    SET_USER_PAYMENT_DATA(state, newData){
+        state.userPaymentData = newData;
+    },
+};
 
 export const actions = {
-    async REGISTER_USER ({ commit }, requestData) {
-        return new Promise ((resolve, reject) => {
+    async REGISTER_USER({commit}, requestData){
+        return new Promise((resolve, reject) => {
             UserApi.registerUser(requestData)
-                .then(response => {
-                    resolve(response)
-                })
-                .catch(error => {
-                    reject(error)
-                })
+                   .then(response => {
+                       resolve(response);
+                   })
+                   .catch(error => {
+                       reject(error);
+                   });
         })
     },
-
-    async LOGIN_USER ({ commit }, requestData) {
-        return new Promise ((resolve, reject) => {
+    async LOGIN_USER({commit}, requestData){
+        return new Promise((resolve, reject) => {
             UserApi.loginUser(requestData)
-                .then(response => {
-                    resolve(response)
-                })
-                .catch(error => {
-                    reject(error)
-                })
+                   .then(response => {
+                       resolve(response)
+                   })
+                   .catch(error => {
+                       reject(error)
+                   });
         })
     },
-
-    async LOGOUT_USER ({ commit }, token) {
-        return new Promise ((resolve, reject) => {
+    async LOGOUT_USER({commit}, token){
+        return new Promise((resolve, reject) => {
             UserApi.logoutUser(token)
-                .then(response => {
-                    commit('SET_USER', null)
-                    resolve(response)
-                })
-                .catch(error => {
-                    reject(error)
-                })
+                   .then(response => {
+                       commit("SET_USER", null);
+                       resolve(response);
+                   })
+                   .catch(error => {
+                       reject(error);
+                   });
         })
     },
-    
-    async LOAD_USER ({ commit }, { id, token }) {
-        return new Promise ((resolve, reject) => {
-            UserApi.loadUser({ id, token })
-                .then(response => {
-                    commit('SET_USER', response.data)
-                    resolve(response)
-                })
-                .catch(error => {
-                    reject(error)
-                })  
+    async LOAD_USER({commit}, {id, token}){
+        return new Promise((resolve, reject) => {
+            UserApi.loadUser({id, token})
+                   .then(response => {
+                       commit("SET_USER", response.data);
+                       resolve(response);
+                   })
+                   .catch(error => {
+                       reject(error);
+                   });
+        });
+    },
+    async UPDATE_USER({commit}, {id, token, params}){
+        return new Promise((resolve, reject) => {
+            UserApi.updateUser({id, token, params})
+                   .then(response => {
+                       commit("SET_USER", response.data);
+                       resolve(response);
+                   })
+                   .catch(error => {
+                       reject(error);
+                   });
         })
     },
-
-    async UPDATE_USER ({ commit }, { id, token, params }) {
-        return new Promise ((resolve, reject) => {
-            UserApi.updateUser({ id, token, params })
-                .then(response => {
-                    commit('SET_USER', response.data)
-                    resolve(response)
-                })
-                .catch(error => {
-                    reject(error)
-                })  
-        })
-    },
-
-    async VERIFY_USER_EMAIL ({ commit }, requestData) {
-        return new Promise ((resolve, reject) => {
+    async VERIFY_USER_EMAIL({commit}, requestData){
+        return new Promise((resolve, reject) => {
             UserApi.verifyUserEmail(requestData)
-                .then(response => {
-                    resolve(response)
-                })
-                .catch(error => {
-                    reject(error)
-                })  
+                   .then(response => {
+                       resolve(response);
+                   })
+                   .catch(error => {
+                       reject(error);
+                   });
         })
     },
-
-    async SEND_EMAIL_VERIFICATION_LINK ({ commit }, requestData) {
-        return new Promise ((resolve, reject) => {
+    async SEND_EMAIL_VERIFICATION_LINK({commit}, requestData){
+        return new Promise((resolve, reject) => {
             UserApi.sendEmailVerifyLink(requestData)
-                .then(response => {
-                    resolve(response)
-                })
-                .catch(error => {
-                    reject(error)
-                })
+                   .then(response => {
+                       resolve(response);
+                   })
+                   .catch(error => {
+                       reject(error);
+                   });
         })
     },
-}
-
+    async LOAD_AND_SAVE_PAYMENT_DATA({commit}, requestData){
+        return new Promise((resolve, reject) => {
+            UserApi.loadPaymentData(requestData)
+                   .then(response => {
+                       commit("SET_USER_PAYMENT_DATA", response.data);
+                       resolve(response);
+                   })
+                   .catch(error => {
+                       reject(error);
+                   });
+        });
+    }
+};
 
 export const getters = {
-    USER (state) {
-        return state.user
+    USER(state){
+        return state.user;
     },
-}
+};
