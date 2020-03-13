d1e5bdfed791023124dbab1c44aff38de7483c26
Vitaliy <vitalii@skynix.co>
2020-03-13 13:04:03 +0200

    OH-151 Doc: /enquiries (add components - drop-down)

--- components/Enquiries/EnquiriesTable.vue
+++ components/Enquiries/EnquiriesTable.vue
@@ -25,7 +25,7 @@
 					<div class = "table__main-item" data-title="Last Name">Jane</div>
 					<div class = "table__main-item" data-title="Enquiry Date">dd/mm/yyyy hh:mm </div>
 					<div class = "table__main-item" data-title="Last Contact">dd/mm/yyyy hh:mm </div>
-					<div class = "table__main-item" data-title="Status">awaiting patient's response</div>
+					<div class = "table__main-item" data-title="Status"><select2 /></div>
 				</NuxtLink>
 				<NuxtLink :to="$routes" class = "table__main-items">
 					<div class = "table__main-item" data-title="Enquiry ID">2</div>
@@ -33,9 +33,74 @@
 					<div class = "table__main-item" data-title="Last Name">Jane</div>
 					<div class = "table__main-item" data-title="Enquiry Date">dd/mm/yyyy hh:mm</div>
 					<div class = "table__main-item" data-title="Last Contact">dd/mm/yyyy hh:mm</div>
-					<div class = "table__main-item" data-title="Status">awaiting patient's response</div>
+					<div class = "table__main-item" data-title="Status"><select2 /></div>
 				</NuxtLink>
 			</div>
 		</div>
 	</div>
 </template>
+
+<script>
+    import select2 from "~/components/select2/select2.vue"
+    export default {
+        components : {
+            select2,
+        },
+    }
+</script>
+<style lang="scss" scoped>
+
+	@include tablet-big-min--big {
+		.table{
+			&__header {
+				display: none;
+				position: relative;
+				padding-bottom: 10px;
+			}
+			&__main-item:not(:last-child) {
+				border-bottom: 1px solid #f3f3f3;
+			}
+			&__main-items {
+				min-height: 112px;
+				color: #7a7d84;
+				display: flex;
+				position: relative;
+				padding-top: 10px;
+				border-radius: 4px;
+				padding-bottom: 10px;
+				-webkit-box-orient: vertical;
+				-webkit-box-direction: normal;
+				flex-direction: column;
+
+				&:after {
+					right: 0;
+					width: 100%;
+				}
+			}
+			&__main-item {
+				position     : relative;
+				flex-basis   : 25%;
+				padding-left : 50%;
+
+
+				&:before {
+					top           : 0;
+					left          : 6px;
+					width         : 45%;
+					color         : $color-curious-blue;
+					display       : block;
+					content       : attr(data-title);
+					position      : absolute;
+					white-space   : nowrap;
+					padding-right : 10px;
+
+
+				}
+			}
+
+		}
+
+
+	}
+</style>
+
