7bce3c730150775daa895916ad13b186648ef35e
Kirill <lambnelsen@gmail.com>
2020-03-19 18:21:13 +0200

    OH-170 - enquiry submission chat - fix error of radiobuttons questions

--- pages/forschung/index.vue
+++ pages/forschung/index.vue
@@ -597,7 +597,13 @@
 
                 this.deleteFromNextQuestionsIdQueue(question_id);
 
-                isLoadNextQuestion && this.loadNextQuestion();
+                setTimeout(() => {
+					if(isLoadNextQuestion){
+						this.loadNextQuestion();
+					} else{
+                        return false;
+					}
+				}, ANIMATION_DURATION);
             },
 
             // Edit message listeners
@@ -644,7 +650,7 @@
                 }
             },
             onEditMessageSubmit(){
-                let editedUserAnswer        = Object.assign({}, this.userAnswers[this.editingData.message_index]);
+                 let editedUserAnswer        = Object.assign({}, this.userAnswers[this.editingData.message_index]);
                 const targetEditingQuestion = this.questions[this.editingData.message_index];
 
                 switch(this.editingData.type){
