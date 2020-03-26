24d425811e416cd7ec53d4b5d3843b82aa2a5cec
Alex <alex.griban@skynix.co>
2020-03-26 13:10:43 +0200

    OH-227-case-page-doc-enquiries-enquiry_id-functionality

---
+++ services/api/Enquires.js
@@ -0,0 +1,68 @@
+import envConfig from "~/configs/env.js";
+import {HTTP} from "~/plugins/modules/axios";
+
+export default {
+    /**
+     * Load Doctors
+     * @return {Promise}
+     */
+    async loadDoctors(requestConfig){
+        const DEFAULT_REQUEST_PARAM = {
+            order_by  : "id",
+            direction : "desc",
+        };
+
+        return new Promise((resolve, reject) => {
+            HTTP.get("/enquires", {params : Object.assign(DEFAULT_REQUEST_PARAM, requestConfig || {})})
+                .then(response => {
+                    const responseData = {
+                        success : true,
+                        status  : response.status,
+                        data    : response.data,
+                        message : 'Enquires success loaded.',
+                    };
+
+                    resolve(responseData);
+                })
+                .catch(error => {
+                    let message = '';
+
+                    if(error.response.status === 500){
+                        message = "Internal technical error was happened.";
+                    }
+
+                    const responseData = {
+                        success : false,
+                        status  : error.response.status,
+                        message,
+                    };
+
+                    reject(responseData);
+                });
+        });
+    },
+
+    async loadEnquire(requestData){
+        return new Promise((resolve, reject) => {
+            HTTP.get(`/enquires/${requestData.enquire_id}`, {
+            params  : requestData.requestData,
+            headers : {
+                "Authorization" : `Bearer ${requestData.token}`,
+                "Content-Type"  : "application/x-www-form-urlencoded"
+            }
+            }).then(response => {
+                resolve({
+                        success : true,
+                        status  : response.status,
+                        data    : response.data,
+                });
+            }).catch(error => {
+                    reject({
+                               success : false,
+                               status  : error.response.status,
+                               message : error.message,
+                          });
+            });
+        });
+    },
+}
