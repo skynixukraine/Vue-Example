45ad1e455a1590be9d24dd03e14fa149c37a69cd
Kirill <lambnelsen@gmail.com>
2020-04-07 14:44:21 +0300

    OH-339 - merge ranch rename all pages - done commit 2

---
+++ layouts/error.vue
@@ -0,0 +1,28 @@
+<template>
+<div class="layout layout-error">
+  <div class="container">
+    <h1 v-if="error.statusCode === 404">Seite nicht gefunden</h1>
+    <h1 v-else>Error</h1>
+    <p><nuxt-link to="/">Back to the home page</nuxt-link></p>
+  </div>
+  </div>
+</template>
+
+<script>
+export default {
+  head(){
+            return {title : this.$t("page-error.head.title")}
+        },
+  props: ['error']
+}
+</script>
+
+<style lang="scss" scoped>
+.layout-error {
+    display: flex;
+    align-items: center;
+    justify-content: center;
+    height: 100vh;
+    text-align: center;
+}
+</style>
