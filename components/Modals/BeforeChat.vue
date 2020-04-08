adde197c9af1f755a443b246731b5f54c46839bf
Артем Скрипка <artemskripka@MacBook-Air.local>
2020-04-08 17:27:11 +0300

    OH-457 gdpr popup make link open in a new tab

--- components/Modals/BeforeChat.vue
+++ components/Modals/BeforeChat.vue
@@ -9,7 +9,7 @@
 		<button class = "modal__close-button" @click = "closeModal($modals.beforeChat)"></button>
 		<div class = "modal__main">
 			<h3>{{ this.$t('before-chat-modal.title') }}</h3>
-			<p>{{ this.$t('before-chat-modal.text1') }}<a href = "#" @click.stop = "goToLink" >{{ this.$t('before-chat-modal.link') }}</a>{{ this.$t('before-chat-modal.text2') }}</p>
+			<p>{{ this.$t('before-chat-modal.text1') }}<a href = "/datenschutz-gmbh" target="_blank">{{ this.$t('before-chat-modal.link') }}</a>{{ this.$t('before-chat-modal.text2') }}</p>
 		</div>
 		<footer class = "modal__footer">
 			<button class = "control-btn--cancel"
@@ -36,10 +36,6 @@
         openDiagnosticChat(){
             this.closeModal(this.$modals.beforeChat);
             this.$store.state.modals.modalMessageHTML();
-        },
-        goToLink(){
-            this.closeModal(this.$modals.beforeChat);
-            this.$router.push(this.$routes.datenschutzGmbh.path);
         }
     }
     }
