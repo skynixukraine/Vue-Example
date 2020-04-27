addf4adb2ffeb5c0198da333a7b600fda695f5c3
Артем Скрипка <artemskripka@MacBook-Air.local>
2020-04-27 11:03:12 +0300

    OH 521 safari issue of displaying

--- pages/datenschutz-gmbh/index.vue
+++ pages/datenschutz-gmbh/index.vue
@@ -1,7 +1,7 @@
 <template>
     <div class="page">
         <div class = "section gmbh-section"
-             :style = "{ backgroundImage: `url(${require('~/static/images/bg/abstract-bg-new.webp')})`}"
+             :style = "{ backgroundImage: `url(${require('~/static/images/bg/abstract-bg-new.jpg')})`}"
         >
             <div class = "container container__mobile-adaptation">
                 <h1 class ="gmbh-title title">
--- pages/ueber-uns/index.vue
+++ pages/ueber-uns/index.vue
@@ -1,7 +1,7 @@
 <template>
     <div class="page">
         <div class = "section ueber-uns-section"
-             :style = "{ backgroundImage: `url(${require('~/static/images/bg/abstract-bg-new.webp')})`}"
+             :style = "{ backgroundImage: `url(${require('~/static/images/bg/abstract-bg-new.jpg')})`}"
         >
             <div class = "container container__mobile-adaptation">
                 <h1 class ="ueber-uns-title title">{{this.$t('ueber-uns.head.title1')}}</h1>
